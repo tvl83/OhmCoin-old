2e780cde634405a5dd6ef9e7259ad69622f332bd
Remapper <33461506+Remapper@users.noreply.github.com>
2017-11-13 21:25:16 +0000

    Masternode amount

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -155,7 +155,7 @@ void ProcessMessageMasternode(CNode* pfrom, std::string& strCommand, CDataStream
 
         CValidationState state;
         CTransaction tx = CTransaction();
-        CTxOut vout = CTxOut(4999*COIN, darkSendPool.collateralPubKey);
+        CTxOut vout = CTxOut(2999*COIN, darkSendPool.collateralPubKey);
         tx.vin.push_back(vin);
         tx.vout.push_back(vout);
         //if(AcceptableInputs(mempool, state, tx)){
@@ -598,7 +598,7 @@ void CMasterNode::Check()
     if(!unitTest){
         CValidationState state;
         CTransaction tx = CTransaction();
-        CTxOut vout = CTxOut(4999*COIN, darkSendPool.collateralPubKey);
+        CTxOut vout = CTxOut(2999*COIN, darkSendPool.collateralPubKey);
         tx.vin.push_back(vin);
         tx.vout.push_back(vout);
 
